37f283b434c809a0938eb50a9632572e466fcbc1
eiedouno <mc_master47@outlook.com>
2026-01-11 20:55:47 -0600

    Added unknown package handling

--- src/ssl.bash
+++ src/ssl.bash
@@ -1,12 +1,16 @@
 main() {
     local inst=$(jq -r ".${1}" "$ssl")
-    local tmppwd="$PWD"
+    if [[ "$inst" == "null" ]]; then
+	printf "${C_ERR}Unknown package: $@.$C_RS\n${C_B}Try updating the Shuttle Script Library. ('shuttle -y')\n$C_RS"
+	exit 1
+    fi
 
     mkdir -p "$HOME/.cache/shuttle/downloads"
     cd "$HOME/.cache/shuttle/downloads"
     rm -rf "$1" > /dev/null
     git clone -q "$inst"
     source ./src/install.bash "$1"
+    rm -rf "$1" > /dev/null
 }
 
 if [[ -f "$ssl" ]]; then
--- src/update_l.bash
+++ src/update_l.bash
@@ -4,4 +4,4 @@ if [[ "$?" != "0" ]]; then
     printf "${C_ERR}Failed to update the Shuttle Script Library.$C_RS\n${C_B}Internet?\nIs it slow?\n$C_RS"
     exit 1
 fi
-printf "${C_B}Successfully updated the library.\n$C_RS"
+printf "${C_G}Successfully updated the library.\n$C_RS"
