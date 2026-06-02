0ba3da97f646e8e6026c1568f69f864fc4c437c1
eiedouno <mc_master47@outlook.com>
2026-06-02 11:15:32 -0500

    fixed uninstall checks

--- src/uninstall.bash
+++ src/uninstall.bash
@@ -1,4 +1,6 @@
 main() {
+    [[ -z "$1" ]] && epln "Specify something to uninstall." && exit 1
+
     if [[ -f "/usr/local/bin/$1" ]]; then
         if rm "/usr/local/bin/$1"; then
             pln "${C_P}Successfully uninstalled $1.\n$C_RS"
@@ -6,12 +8,12 @@ main() {
             bruh
         fi
     else
-        epln "File /usr/local/bin/$1 is already uninstalled."
+        epln "File /usr/local/bin/$1 does not exist."
     fi
 }
 
 bruh() {
-    epln "Unable to uninstall." "Try running as root or changing the ownership of '/usr/local/bin'."
+    epln "Unable to uninstall." "Try changing the ownership of '/usr/local/bin'.\n\nsudo chown eiedouno:eiedouno /usr/local/bin"
     exit 1
 }
 
